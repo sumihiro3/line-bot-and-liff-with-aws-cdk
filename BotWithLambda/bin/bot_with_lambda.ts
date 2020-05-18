be339f86f34debc20947aefddff4860f771d9e89
sumihiro3 <sumihiro@gmail.com>
2020-05-18 23:03:25 +0900

    apply prettier

--- BotWithLambda/bin/bot_with_lambda.ts
+++ BotWithLambda/bin/bot_with_lambda.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { BotWithLambdaStack } from '../lib/bot_with_lambda-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { BotWithLambdaStack } from '../lib/bot_with_lambda-stack'
 import { prepareLambdaLayerModules } from '../lib/prepareLambdaLayerModules'
 
 console.log('ACCESS_TOKEN', process.env.ACCESS_TOKEN)
 console.log('CHANNEL_SECRET', process.env.CHANNEL_SECRET)
 
-prepareLambdaLayerModules();
+prepareLambdaLayerModules()
 
-const app = new cdk.App();
-new BotWithLambdaStack(app, 'BotWithLambdaStack');
+const app = new cdk.App()
+new BotWithLambdaStack(app, 'BotWithLambdaStack')
